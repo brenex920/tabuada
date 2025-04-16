f004efed84fb516d2fc2d45819ca7e3dff65eedb
BRENO DIAS MACHADO <25132475@CFP127ALUNO.LOCAL>
2025-04-16 16:56:23 -0300

    começo dos calculos

--- src/br/dev/breno/tabuada/gui/TelaTabuada.java
+++ src/br/dev/breno/tabuada/gui/TelaTabuada.java
@@ -1,11 +1,17 @@
 package br.dev.breno.tabuada.gui;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+
+import model.tabuada;
 
 public class TelaTabuada {
 
@@ -33,40 +39,55 @@ public class TelaTabuada {
 		
 		JFrame tela = new JFrame();
 		tela.setTitle("tabuada magica");
-		tela.setSize(400, 700);
+		tela.setSize(270, 700);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setLayout(null);
-		
+		tela.setResizable(false);
 		
 
 		
 		//vamos criar os componentes da tela
 		labelMutiplicado = new JLabel();
 		labelMutiplicado.setText("Mutiplicandor: ");
-		labelMutiplicado.setBounds(20, 50, 150, 30);
+		labelMutiplicado.setBounds(20, 20, 150, 30);
 		
 		
 		labelMinimoMutiplicador = new JLabel();
 		labelMinimoMutiplicador.setText("Min. multiplicador: ");
-		labelMinimoMutiplicador.setBounds(20, 80, 150, 30);
+		labelMinimoMutiplicador.setBounds(20, 65, 150, 30);
 		
 		
 		
 		labelMaximoMutiplicador = new JLabel();
 		labelMaximoMutiplicador.setText("Max. multiplicador: ");
-		labelMaximoMutiplicador.setBounds(20, 140, 150, 30);
+		labelMaximoMutiplicador.setBounds(20, 100, 150, 30);
 		
 		
 		pexMutiplicador = new JTextField();
-		pexMutiplicador.setBounds(300, 20, 50, 40);
+		pexMutiplicador.setBounds(180, 20, 50, 30);
 		
 		texMinimoMutiplicador = new JTextField();
-		texMinimoMutiplicador.setBounds(300, 80, 50, 40);
+		texMinimoMutiplicador.setBounds(180, 65, 50, 30);
 		
 		texMaximoMutiplicador = new JTextField();
-		texMaximoMutiplicador.setBounds(300, 140, 50, 40);
+		texMaximoMutiplicador.setBounds(180, 100, 50, 30);
+		
+		buttonCalcular = new JButton();
+		buttonCalcular.setText("calcular");
+		buttonCalcular.setBounds(20, 155, 95, 30);
+		
+		buttonLimpar = new JButton();
+		buttonLimpar.setText("limpar");
+		buttonLimpar.setBounds(135, 155, 95, 30);
+		
+		//criar um objeto scroll
+		listTabuada = new JList();
 		
 		
+		
+		
+		scrollTabuada = new JScrollPane(listTabuada);
+		scrollTabuada.setBounds(20, 205, 210, 400);
 		
 		//Obtemos o painel de conteudo e adicionamos o labelMutiplicando nesse painel
 				tela.getContentPane().add(labelMutiplicado);
@@ -75,6 +96,61 @@ public class TelaTabuada {
 				tela.getContentPane().add(pexMutiplicador);
 				tela.getContentPane().add(texMinimoMutiplicador);
 				tela.getContentPane().add(texMaximoMutiplicador);
+				tela.getContentPane().add(buttonCalcular);
+				tela.getContentPane().add(buttonLimpar);
+				tela.getContentPane().add(scrollTabuada);
+				
+				//adicionar ouvintes de eventos aos botões
+				
+				buttonCalcular.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						
+						tabuada tabuada = new tabuada();
+						String multiplicando = pexMutiplicador.getText();
+						double multiplicandoDouble = Double.valueOf(multiplicando);
+						tabuada.setMutiplicando(multiplicandoDouble);
+						
+						String min = texMinimoMutiplicador.getText();
+						double minDouble = Double.valueOf(min);
+						tabuada.setMinimoMultiplicador(minDouble);
+						
+						String max =
+						
+						
+						
+						
+						
+						
+						System.out.println(pexMutiplicador.getText() );
+						System.out.println(texMinimoMutiplicador.getText() );
+						System.out.println(texMaximoMutiplicador.getText() );
+						
+					}
+				});
+				
+				
+				//esse botao faz a açao de limpar 
+				buttonLimpar.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+					
+						pexMutiplicador.setText(" ");
+						texMinimoMutiplicador.setText(" ");
+						texMaximoMutiplicador.setText(" ");
+						
+						pexMutiplicador.setFocusable(false);;
+						
+					}
+				});
+				
+				
+				
+				
+				
+				
 		//essa linha deve ser a  utima linha desse metodo
 		tela.setVisible(true);
 		
